d3fdd00bdf1a1662b1eee96c04b0db913e7b1e0b
Minh Nguyen <nnnnguyen43@gmail.com>
2025-12-08 15:30:58 +0700

    update config

--- config/Database.php
+++ config/Database.php
@@ -1,2 +1,28 @@
 <?php
-// Database.php
+
+class Database {
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct() {
+        $config = require __DIR__ . '/config.php';
+        try {
+            $dsn = "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8mb4";
+            $this->pdo = new PDO($dsn, $config['username'], $config['password']);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
